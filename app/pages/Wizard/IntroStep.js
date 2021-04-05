f4cb44ed0519c195863e8f68c644efeecd2a5d34
CapDog <34176400+capitalistdog@users.noreply.github.com>
2021-04-05 13:43:23 -0400

    Update IntroStep.js

--- app/pages/Wizard/IntroStep.js
+++ app/pages/Wizard/IntroStep.js
@@ -7,16 +7,12 @@ export default class IntroStep extends Component {
     return (
       <div className={styles.container}>
         <div className={styles.wizardStepContainer}>
-          <span className={styles.warning}>
-            Note that this is alpha software and there are likely to be bugs, so do not risk more
-            funds than you are willing to lose in testing.
-          </span>
           <p className={styles.stepDescription}>
             Welcome to the ShockWallet Wizard.
             <br />
             <br />
             This tool will walk you through the setup of your own Lightning node for use with the
-            ShockWallet App, and if desired, a fully-validating Bitcoin Core node.
+            Shockwallet App, and if desired, a fully-validating Bitcoin Core node.
             <br />
             <br />
             Please submit any issues or requests for support at{' '}
